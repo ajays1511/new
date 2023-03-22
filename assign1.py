93ccf83f89bb3d28a77d1e914f22f1178a8fc370
ajays1511 <128451064+ajays1511@users.noreply.github.com>
2023-03-22 16:12:40 +0530

    Add files via upload

---
+++ assign1.py
@@ -0,0 +1,47 @@
+# ques.1
+
+aa=("hello",[10,20,30],15)
+print(aa[1])
+bb=aa[1]
+print (bb[1])
+
+
+
+
+
+
+#
+#
+# #ques.2
+#
+# tup1=(10,20,30,40)
+# for i in range(len(tup1)):
+#     print("mark",i+1,tup1[i])
+#
+#
+#
+#
+#
+#
+#
+# #ques.3
+#
+# l=[1,2,3,4]
+# sum=0
+# for i in range(len(l)):
+#     sum=sum+l[i]
+# print("sum=",sum)
+#
+#
+#
+#
+#
+#
+# #ques.4
+#
+# a=[1,2,3,4,5]
+# b=[]
+# for i in range(len(a)):
+#     x=a[i]*a[i]
+#     b.append(x)
+# print(b)
